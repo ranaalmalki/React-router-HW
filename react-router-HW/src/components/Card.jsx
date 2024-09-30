4dc417e0daf047ff92e483d8b8c8e1ec6f1f2182
Rana Almalki <ranaalmalki12@gmail.com>
2024-09-30 08:34:03 +0300

    edit the responsive and add hover bg in the card

--- react-router-HW/src/components/Card.jsx
+++ react-router-HW/src/components/Card.jsx
@@ -3,38 +3,38 @@ import { Link } from 'react-router-dom'
 
 function Card(props) {
   return (
-    <div className=' flex justify-center gap-7  lg:flex-row md:flex-col md:items-center'>
+    <div className=' flex justify-center gap-7  lg:flex-row md:flex-col md:items-center sm:flex-col sm:items-center text-black'>
        
-        <div className='Card shadow-md shadow-white w-64 h-48 flex justify-center items-center bg-white'>
-            <div className='card-body flex items-center bg-white'>
-               <h2 className='card-title'> <Link to="/desert">{props.title}</Link></h2>
+        <div className='Card shadow-md shadow-white w-64 h-48 flex justify-center items-center bg-white hover:bg-orange-200'>
+            <div className='card-body flex items-center bg-white hover:bg-orange-200'>
+               <h2 className='card-title '> <Link to="/desert">{props.title}</Link></h2>
 
             </div>
 
         </div>
-        <div className='Card shadow-md shadow-white w-64 h-48 flex justify-center items-center bg-white'>
-            <div className='card-body flex items-center bg-white'>
-               <h2 className='card-title'><Link to="/forest">{props.title2}</Link></h2>
+        <div className='Card shadow-md shadow-white w-64 h-48 flex justify-center items-center bg-white hover:bg-lime-100'>
+            <div className='card-body flex items-center bg-white hover:bg-lime-100'>
+               <h2 className='card-title '><Link to="/forest">{props.title2}</Link></h2>
 
             </div>
 
         </div>
-        <div className='Card shadow-md shadow-white w-64 h-48 flex justify-center items-center bg-white'>
-            <div className='card-body flex items-center bg-white'>
+        <div className='Card shadow-md shadow-white w-64 h-48 flex justify-center items-center bg-white hover:bg-cyan-200'>
+            <div className='card-body flex items-center bg-white hover:bg-cyan-200'>
                <h2 className='card-title'><Link to="/mountains">{props.title3}</Link></h2>
 
             </div>
 
         </div>
-        <div className='Card shadow-md shadow-white w-64 h-48 flex justify-center items-center bg-white'>
-            <div className='card-body flex items-center bg-white'>
+        <div className='Card shadow-md shadow-white w-64 h-48 flex justify-center items-center bg-white hover:bg-sky-200'>
+            <div className='card-body flex items-center bg-white hover:bg-sky-200'>
                <h2 className='card-title'><Link to="/sea">{props.title4}</Link></h2>
 
             </div>
 
         </div>
-        <div className='Card shadow-md shadow-white w-64 h-48 flex justify-center items-center bg-white'>
-            <div className='card-body flex items-center bg-white'>
+        <div className='Card shadow-md shadow-white w-64 h-48 flex justify-center items-center bg-white hover:bg-green-200'>
+            <div className='card-body flex items-center bg-white hover:bg-green-200'>
                <h2 className='card-title'><Link to="/valley">{props.title5}</Link></h2>
 
             </div>
--- react-router-HW/src/components/Homepage.jsx
+++ react-router-HW/src/components/Homepage.jsx
@@ -1,10 +1,10 @@
 import Card from "./Card"
 function Homepage(props) {
   return (
-    <div className="bg-cover  p-48 relative flex flex-col gap-20 h-screen " style={{backgroundImage:`url(${props.img})`}} >
+    <div className="bg-cover  p-48 relative flex flex-col gap-20 lg:h-screen md:min-h-screen " style={{backgroundImage:`url(${props.img})`}} >
  
-    <div className="card shadow-lg shadow-black flex h-64 bg-green-50  items-center justify-center text-center ">
-        <div className="card-body w-full h-full">
+    <div className="card shadow-lg shadow-black flex  bg-green-50  items-center justify-center text-center ">
+        <div className="card-body w-full ">
     <h1 className=" text-black text-3xl font-bold">{props.name}</h1>
     <p className="text-black text-xl">{props.content}</p>
     </div>
